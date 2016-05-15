1af8bfdd03061c65065fe6424a116b8056ccf0de
Red-French <red@redfrench.net>
2016-05-15 02:06:20 -0500

    data populating DOM

--- public/controllers/SingerCtrl.js
+++ public/controllers/SingerCtrl.js
@@ -8,35 +8,13 @@ app.controller('SingerCtrl', ['$scope', '$http', 'dataService',
 // INITIAL LOAD OF SONGS
 dataService.getSingers(function(response){  // 'response' from getLeaders function in services/data.js
 console.log(response);
-  var singers = response.data.singer;  // get 'singers' data from getLeaders 'response'
-  $scope.singers =  singers;  // put data on $scope for ng-repeat in DOM
+console.log(response.data);
+console.log(response.data.passfail);
+  var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
+  $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
 });
 
-$scope.newSong = { song: "", singer: "", status: ""};  // create new object to hold user input
-
-$scope.addSong = function() {
-  // $scope.songs.$add({  // add user input to Firebase
-  //   song: $scope.newSong.song,
-  //   singer: $scope.newSong.singer,
-  //   status: $scope.newSong.status
-  // });
-  $('#addSongModal').modal('show');
-};
 
 }]);
-// app.controller('SingerCtrl', ['$scope', '$http', 'dataService',
-//   function($scope, $http, dataService) {
-//
-//     $scope.newSong = { artist: "", album: "", title: ""};  // create new object to hold user input
-//
-//     $scope.addSong = function() {
-//       $scope.songs.$add({  // add user input to Firebase
-//         artist: $scope.newSong.artist,
-//         title: $scope.newSong.title,
-//         album: $scope.newSong.album
-//       });
-//       $('#addSongModal').modal('show');
-//     };
-//   }
-// ]);
+
 })();
