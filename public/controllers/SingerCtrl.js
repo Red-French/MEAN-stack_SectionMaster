f2e2d4be0895378ac6bc1973f37106018ed7aa9d
Red-French <red@redfrench.net>
2016-05-15 16:54:58 -0500

    add function to prevent duplicates in dropdowns

--- public/controllers/SingerCtrl.js
+++ public/controllers/SingerCtrl.js
@@ -1,20 +1,34 @@
 (function() {
 'use strict';
+
 const app = angular.module('MNCApp');
+
+// remove duplicates from ng-repeat list
+app.filter('unique', function() {
+   return function(collection, keyname) {
+      var output = [],
+          keys = [],
+          key = null;
+
+      angular.forEach(collection, function(item) {
+          key = item[keyname];
+          if(keys.indexOf(key) === -1) { // if does not already exist
+              keys.push(key);
+              output.push(item);
+          }
+      });
+      return output;  // output with no duplicates
+   };
+});
+
 app.controller('SingerCtrl', ['$scope', '$http', 'dataService',
   function($scope, $http, dataService) {
     $scope.dataService = dataService;
 
-// INITIAL LOAD OF SONGS
-dataService.getSingers(function(response){  // 'response' from getLeaders function in services/data.js
-console.log(response);
-console.log(response.data);
-console.log(response.data.passfail);
-  var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
-  $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
-});
-
-
-}]);
-
+    // INITIAL LOAD OF SONGS
+    dataService.getSingers(function(response){  // 'response' from getLeaders function in services/data.js
+      var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
+      $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
+    });
+  }]);
 })();
