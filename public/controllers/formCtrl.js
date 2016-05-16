54f2e2d0ebfceab5623cf0325106a7fed2dd5be0
Red-French <red@redfrench.net>
2016-05-15 23:09:12 -0500

    hard-coded data populating local database

--- public/controllers/formCtrl.js
+++ public/controllers/formCtrl.js
@@ -1,16 +1,41 @@
-const app = angular.module('MNCApp');
-app.controller("formCtrl", ["$scope",
-  function($scope) {
+(function() {
+'use strict';
 
-    $scope.newSong = { artist: "", album: "", title: ""};  // create new object to hold user input
+  const app = angular.module('MNCApp');
+  app.controller("formCtrl", ["$scope", "$location", "dataService",
+    function($scope, $location, dataService) {
+      $scope.dataService = dataService;
 
-    $scope.addSong = function() {
-      $scope.songs.$add({  // add user input to Firebase
-        artist: $scope.newSong.artist,
-        title: $scope.newSong.title,
-        album: $scope.newSong.album
-      });
-      $('#addSongModal').modal('show');
-    };
-  }
-]);
+      var songTitle = 'Black Dog';
+
+        dataService.addData({song: songTitle}).then(function(response){  // saveLeaders function in services/data.js
+          console.log('inside formCtrl > addData');
+          console.log(response);
+          dataService.getSingers(function() {
+            var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
+            $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
+          });
+          $('#addInfoModal').modal('show');
+        });
+
+      // dataService.saveLeaders({song: songTitle}).then(function(response){  // saveLeaders function in services/data.js
+      //   dataService.getLeaders(function(response){  // 'response' from getLeaders function in services/data.js
+      //     var leaders = response.data.leaders;  // get 'leaders' data from getLeaders 'response'
+      //     $scope.leaders =  leaders;  // put data on $scope for ng-repeat in DOM
+      //   });
+      // });
+
+
+      // $scope.newSong = { song: "", singer: "", status: ""};  // create new object to hold user input
+      //
+      // $scope.addSong = function() {
+      //   $scope.songs.$add({  // add user input to Firebase
+      //     song: $scope.newSong.song,
+      //     singer: $scope.newSong.singer,
+      //     status: $scope.newSong.status
+      //   });
+      //   $('#addSongModal').modal('show');
+      // };
+    }
+  ]);
+})();
