87a956896d40634a1aeec9e50a396f1524bf766d
Red-French <red@redfrench.net>
2016-05-15 23:13:55 -0500

    add function to change route for formCtrl

--- public/controllers/SingerCtrl.js
+++ public/controllers/SingerCtrl.js
@@ -1,28 +1,28 @@
 (function() {
 'use strict';
 
-const app = angular.module('MNCApp');
+  const app = angular.module('MNCApp');
 
-// remove duplicates from ng-repeat list
-app.filter('unique', function() {
-   return function(collection, keyname) {
-      var output = [],
-          keys = [],
-          key = null;
+  // remove duplicates from ng-repeat list
+  app.filter('unique', function() {
+     return function(collection, keyname) {
+        var output = [],
+            keys = [],
+            key = null;
 
-      angular.forEach(collection, function(item) {
-          key = item[keyname];
-          if(keys.indexOf(key) === -1) { // if does not already exist
-              keys.push(key);
-              output.push(item);
-          }
-      });
-      return output;  // output with no duplicates
-   };
-});
+        angular.forEach(collection, function(item) {
+            key = item[keyname];
+            if(keys.indexOf(key) === -1) { // if does not already exist
+                keys.push(key);
+                output.push(item);
+            }
+        });
+        return output;  // output with no duplicates
+     };
+  });
 
-app.controller('SingerCtrl', ['$scope', '$http', 'dataService',
-  function($scope, $http, dataService) {
+  app.controller('SingerCtrl', ['$scope', '$http', '$location', 'dataService',
+  function($scope, $http, $location, dataService) {
     $scope.dataService = dataService;
 
     // INITIAL LOAD OF SONGS
@@ -30,5 +30,11 @@ app.controller('SingerCtrl', ['$scope', '$http', 'dataService',
       var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
       $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
     });
+
+    // button on 'add info' page triggers this function
+    $scope.changeview = function () {
+        $location.path('/data/submit');  // change url to trigger controller change (formCtrl)
+    };
   }]);
+
 })();
