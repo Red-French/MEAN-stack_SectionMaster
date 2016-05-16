285085c146ad41a71f0e0c3278129666fbb2f7da
Red-French <red@redfrench.net>
2016-05-15 23:10:19 -0500

    hard-coded data populating local database

--- public/controllers/formCtrl.js
+++ public/controllers/formCtrl.js
@@ -8,34 +8,15 @@
 
       var songTitle = 'Black Dog';
 
-        dataService.addData({song: songTitle}).then(function(response){  // saveLeaders function in services/data.js
-          console.log('inside formCtrl > addData');
-          console.log(response);
-          dataService.getSingers(function() {
-            var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
-            $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
-          });
-          $('#addInfoModal').modal('show');
+      dataService.addData({song: songTitle}).then(function(response){  // saveLeaders function in services/data.js
+        console.log('inside formCtrl > addData');
+        console.log(response);
+        dataService.getSingers(function() {
+          var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
+          $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
         });
-
-      // dataService.saveLeaders({song: songTitle}).then(function(response){  // saveLeaders function in services/data.js
-      //   dataService.getLeaders(function(response){  // 'response' from getLeaders function in services/data.js
-      //     var leaders = response.data.leaders;  // get 'leaders' data from getLeaders 'response'
-      //     $scope.leaders =  leaders;  // put data on $scope for ng-repeat in DOM
-      //   });
-      // });
-
-
-      // $scope.newSong = { song: "", singer: "", status: ""};  // create new object to hold user input
-      //
-      // $scope.addSong = function() {
-      //   $scope.songs.$add({  // add user input to Firebase
-      //     song: $scope.newSong.song,
-      //     singer: $scope.newSong.singer,
-      //     status: $scope.newSong.status
-      //   });
-      //   $('#addSongModal').modal('show');
-      // };
+        $('#addInfoModal').modal('show');
+      });
     }
   ]);
 })();
