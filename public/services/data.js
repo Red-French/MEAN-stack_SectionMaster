d96078ec08747c5c7b7445e57c6eaff0777a7cb1
Red-French <red@redfrench.net>
2016-05-15 23:11:58 -0500

    wrap in IIFE

--- public/services/data.js
+++ public/services/data.js
@@ -1,32 +1,33 @@
 (function() {
 'use strict';
 // SERVICES/DATA (CONNECT TO API BACK-END)
+  const app = angular.module('MNCApp');
+  
+  app.service('dataService', ['$http', function($http, $q) {
 
-app.service('dataService', function($http, $q) {
+    // CREATE
+    this.addData = function(songData) {
+      console.log(songData);
+      console.log('inside service > addSong');
+      return $http.post('/api/status', songData);
+    };
 
-  // CREATE
-  this.addSong = function(songData) {
-    console.log('inside service > addSong');
-    return http.post('/songs/form').then(cb);
-  };
+    // READ
+    this.getSingers = function(callback) {
+      return $http.get('/api/status').then(callback);
+    };
 
-  // READ
-  this.getSingers = function(callback) {
-    console.log('inside service getSingers');
-    return $http.get('/api/status').then(callback);
-  };
+    // UPDATE
+    this.saveSingerData = function(songData) {
+      console.log("I saved " + songData.length + " singers!");
+      return $http.put('/api/status', songData);
+    };
 
-  // UPDATE
-  this.saveSingerData = function(songData) {
-    console.log("I saved " + songData.length + " singers!");
-    return $http.put('/api/status', songData);
-  };
+    // DELETE
+    this.deleteSingerData = function(songData) {
+      console.log("I deleted the singer, " + songData.singer + "!");
+      return $http.delete('/api/status', songData);
+    };
 
-  // DELETE
-  this.deleteSingerData = function(songData) {
-    console.log("I deleted the singer, " + songData.singer + "!");
-    return $http.delete('/api/status', songData);
-  };
-
-});
+  }]);
 })();
