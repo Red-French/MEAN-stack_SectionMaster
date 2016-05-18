ee922e4c523532b59f5d380c8176ad7e6db1943b
Red-French <red@redfrench.net>
2016-05-17 22:09:20 -0500

    data populating mongoDB and DOM

--- public/controllers/formCtrl.js
+++ public/controllers/formCtrl.js
@@ -1,22 +1,29 @@
 (function() {
 'use strict';
 
+  let title = null;
+  let person = null;
+  let standing = null;
+
   const app = angular.module('MNCApp');
   app.controller("formCtrl", ["$scope", "$location", "dataService",
     function($scope, $location, dataService) {
-      $scope.dataService = dataService;
 
-      var songTitle = 'Black Dog';
+      $scope.addinfo = function () {  // button on 'add info' (songForm.html) page triggers this function
+        title = $('#theSong').val();
+        person = $('#theSinger').val();
+        standing= $('#theStatus').val();
 
-      dataService.addData({song: songTitle}).then(function(response){  // saveLeaders function in services/data.js
-        console.log('inside formCtrl > addData');
-        console.log(response);
-        dataService.getSingers(function() {
-          var sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
-          $scope.sectionStats =  sectionStats;  // put data on $scope for ng-repeat in DOM
+        dataService.addData({song: title, singer: person, status: standing}).then(function(response){
+          dataService.getSingers(function(response) {
+            let sectionStats = response.data.passfail;  // get 'singers' data from getLeaders 'response'
+          });
+          $('#addInfoModal').modal('show');
+          $('#theSong').val('');
+          $('#theSinger').val('');
+          $('#theStatus').val('');
         });
-        $('#addInfoModal').modal('show');
-      });
+      };
     }
   ]);
 })();
