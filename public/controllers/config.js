4d94e3c940ea2f3de1a556509cdabbe8707ac3d4
Red-French <red@redfrench.net>
2016-05-17 23:00:44 -0500

    change controller name

--- public/controllers/config.js
+++ public/controllers/config.js
@@ -6,7 +6,7 @@ app.config(['$routeProvider',
    $routeProvider
    .when('/list', {  // when URL ends with this
      templateUrl: 'views/songList.html',
-     controller: 'SingerCtrl'
+     controller: 'loadCtrl'
    })
    .when('/form', {  // when URL ends with this
      templateUrl: 'views/songForm.html',
